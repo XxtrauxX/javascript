5f3c250a1383217256e9f904a078fc1b56352b67
oscar <aaronjones2429@gmail.com>
2024-11-28 00:08:31 -0500

    agragando info 27 no

---
+++ repeticion/forfor.js
@@ -0,0 +1,22 @@
+let str= "campus lands";
+
+let vocales ="aeiou";
+for(let i=0; i < str.length; i++){
+
+    let letra = str[i];
+    if(letra === "a") continue;
+    console.log(letra);
+
+
+}
+
+
+
+// for of
+
+/*
+for (let letra of str) {
+    console.log(letra);
+}
+
+*/
